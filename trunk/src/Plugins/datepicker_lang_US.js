cce20c335dae231d52e6ad6de1a6cdc60ad03230
lispro06@gmail.com <lispro06@gmail.com@a97a581a-b179-fca6-b8e4-b75c4b7648a7>
2013-08-05 23:53:43 +0000

    US to KO

--- trunk/src/Plugins/datepicker_lang_US.js
+++ trunk/src/Plugins/datepicker_lang_US.js
@@ -1,4 +1,4 @@
-﻿var i18n = $.extend({}, i18n || {}, {
+var i18n = $.extend({}, i18n || {}, {
     datepicker: {
         dateformat: {
             "fulldayvalue": "M/d/yyyy",
@@ -6,32 +6,32 @@
             "year_index": 2,
             "month_index": 0,
             "day_index": 1,
-            "sun": "Sun",
-            "mon": "Mon",
-            "tue": "Tue",
-            "wed": "Wed",
-            "thu": "Thu",
-            "fri": "Fri",
-            "sat": "Sat",
-            "jan": "Jan",
-            "feb": "Feb",
-            "mar": "Mar",
-            "apr": "Apr",
-            "may": "May",
-            "jun": "Jun",
-            "jul": "Jul",
-            "aug": "Aug",
-            "sep": "Sep",
-            "oct": "Oct",
-            "nov": "Nov",
-            "dec": "Dec",
+            "sun": "일",
+            "mon": "월",
+            "tue": "화",
+            "wed": "수",
+            "thu": "목",
+            "fri": "금",
+            "sat": "토",
+            "jan": "1월",
+            "feb": "2월",
+            "mar": "3월",
+            "apr": "4월",
+            "may": "5월",
+            "jun": "6월",
+            "jul": "7월",
+            "aug": "8월",
+            "sep": "9월",
+            "oct": "10월",
+            "nov": "11월",
+            "dec": "12월",
             "postfix": ""
         },
-        ok: " Ok ",
-        cancel: "Cancel",
-        today: "Today",
-        prev_month_title: "prev month",
-        next_month_title: "next month"
+        ok: " 완료 ",
+        cancel: "취소",
+        today: "오늘",
+        prev_month_title: "이전달",
+        next_month_title: "다음달"
     }
 });
 
