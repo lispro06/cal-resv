4f6ea073ac5454d0f517ee32bd9454b6549dd8ad
lispro06@gmail.com <lispro06@gmail.com@a97a581a-b179-fca6-b8e4-b75c4b7648a7>
2013-08-07 01:57:47 +0000

    임의 접속이 불가 하도록 설정

--- trunk/edit.php
+++ trunk/edit.php
@@ -1,9 +1,41 @@
 <?php
-include_once("php/dbconfig.php");
+header("Content-Type: text/html; charset=UTF-8");
+//이중 로그인과 db정보 연동을 위한 파일 로딩2013-08-07
+   	define('__ZBXE__', true);
+	include("./files/config/db.config.php");
+	$dbname=$db_info->master_db['db_userid'];
+	$dbpass=$db_info->master_db['db_password'];
+class DBConnection{
+	function getConnection($dbname,$dbpass){
+	  //change to your database server/user name/password
+		mysql_connect("localhost",$dbname,$dbpass) or
+         die("Could not connect: " . mysql_error());
+    //change to your database name
+		mysql_select_db($dbname) or 
+		     die("Could not select database: " . mysql_error());
+	}
+}
+ require_once('./config/config.inc.php');
+ $oContext = &Context::getInstance();
+ $oContext->init();
+    
+ $logged_info = Context::get('logged_info'); 
+ $id = $logged_info->user_id;
+
+if(!$_SESSION['sunap']){//권한이 없으면 종료
+	exit();
+}else{
+	$aclSql="select * from `toto_acl` where `user_id`='".$_SESSION['sunap']."'";
+	$aclRes = mysql_query($aclSql); 
+	$aclRow = mysql_fetch_row($aclRes);
+}
+if($aclRow[8]!="Y"){//권한이 없으면 종료
+	exit();
+}
 include_once("php/functions.php");
 // db 커넥션을 무조건 한다. 2013-08-05
     $db = new DBConnection();
-    $db->getConnection();
+    $db->getConnection($dbname,$dbpass);
 function getCalendarByRange($id){
   try{    
     $sql = "select * from `jqcalendar` where `id` = " . $id;
@@ -17,7 +49,9 @@ function getCalendarByRange($id){
 if($_GET["id"]){
 	$event = getCalendarByRange($_GET["id"]);
 	$cb[$event->CUST_GUBN]="checked";//고객 구분 배열 2013-08-05
+	$reged_date=substr($event->REGI_DATE,0,10);
 }else{//2013-08-05 세부 일정 클릭 시 내용 수신
+	$reged_date=date("Y-m-d",time());
 	$cb["N"]="checked";//새 일정일 경우 고객은 신환으로 설정
 	$title_G = $_GET["title"];
 	$sarr_G = explode(" ", $_GET["start"]);
@@ -80,7 +114,7 @@ if($_GET["id"]){
             var DATA_FEED_URL = "php/datafeed.php";
             var arrT = [];
             var tt = "{0}:{1}";
-            for (var i = 0; i < 24; i++) {
+            for (var i = 0; i < 24; i++) {//combo box로 된 시간 선택 상자
                 arrT.push({ text: StrFormat(tt, [i >= 10 ? i : "0" + i, "00"]) }, { text: StrFormat(tt, [i >= 10 ? i : "0" + i, "30"]) });
             }
             $("#timezone").val(new Date().getTimezoneOffset()/60 * -1);
@@ -209,8 +243,8 @@ if($_GET["id"]){
   <body>    
     <div>      
       <div class="toolBotton">           
-        <a id="Savebtn" class="imgbtn" href="javascript:void(0);">                
-          <span class="Save"  title="예약 입력">저장(<u>S</u>)
+        <a id="Savebtn" class="imgbtn" href="javascript:void(0);">
+          <span class="Save" title="예약 입력" onClick="subj();">저장(<u>S</u>)
           </span>          
         </a>                           
         <?php if(isset($event)){ ?>
@@ -228,36 +262,161 @@ if($_GET["id"]){
       </div>        
       <div class="infocontainer">            
         <form action="php/datafeed.php?method=adddetails<?php echo isset($event)?"&id=".$event->Id:""; ?>" class="fform" id="fmEdit" method="post">
-		<span><b>*고객구분 <input type="radio" name="CUST_GUBN" id="CUST_GUBN" value="N" onchange="srch_on();" <?php echo $cb["N"];?> />신환 <input type="radio" name="CUST_GUBN" id="CUST_GUBN" value="O" <?php echo $cb["O"];?> onchange="srch_on();" />구환&nbsp;&nbsp;&nbsp; 고객검색 <input type="text" disabled size="10" name="iden" id="iden"></input><input type="text" size="10" id="keyword" name="keyword" value="" disabled></input><input type="button" onclick="cust_srch();" value="검색" id="sc_bt" name="sc_bt" disabled></input>
+		<div style="border:1px solid #333333;padding:2px;">
+		<span><b>&nbsp;<u>고객구분</u> <input type="radio" name="CUST_GUBN" id="CUST_GUBN" value="N" onchange="srch_on();" <?php echo $cb["N"];?> />신환 <input type="radio" name="CUST_GUBN" id="CUST_GUBN" value="O" <?php echo $cb["O"];?> onchange="srch_on();" />구환&nbsp;&nbsp;&nbsp; 고객검색 <input type="text" disabled size="10" name="iden" id="iden"></input><input type="text" size="10" id="keyword" name="keyword" value="" disabled></input><input type="button" onclick="cust_srch();" value="검색" id="sc_bt" name="sc_bt" disabled></input>
 		</span><br />
-		  <span><b>*고객성명: <input class="required safe" name="CUST_NAME" id="CUST_NAME" size="7" value="<?php echo $event->CUST_NAME;?>"></input>&nbsp;&nbsp;&nbsp; *연락처: <input class="required safe" name="CUST_TELE" id="CUST_TELE" size="15" value="<?php echo $event->CUST_TELE;?>"></input>&nbsp;&nbsp;&nbsp; 주민번호: <input name="CUST_IDEN" id="CUST_IDEN" size="15" value="<?php echo $event->CUST_CNUM;?>"></input></b></span>
-		  <label>
-            <span>                        *제목:
-            </span>                    
-            <div id="calendarcolor">
-            </div>
-            <input MaxLength="200" class="required safe" id="Subject" name="Subject" style="width:85%;" type="text" value="<?php echo isset($event)?$event->Subject:"" ?><?php echo $title_G;?>" />                     
-            <input id="colorvalue" name="colorvalue" type="hidden" value="<?php echo isset($event)?$event->Color:"" ?>" />                
-          </label>                 
-          <label>                    
-            <span>*일시:
-            </span>                    
-            <div>  
+		  <span><b>&nbsp;<u>고객성명</u>&nbsp;&nbsp;<input class="required safe" name="CUST_NAME" id="CUST_NAME" size="7" value="<?php echo $event->CUST_NAME;?>"></input>&nbsp;&nbsp;&nbsp; <u>연&nbsp;락&nbsp;처</u> <input class="required safe" name="CUST_TELE" id="CUST_TELE" size="15" value="<?php echo $event->CUST_TELE;?>"></input>&nbsp;&nbsp; 주&nbsp;민&nbsp;번&nbsp;호 <input name="CUST_IDEN" id="CUST_IDEN" size="15" value="<?php echo $event->CUST_CNUM;?>" disabled></input></b></span><br />
+		  <table><tr><td>고객메모<br />(특이사항)</td><td><textarea id="CUST_MEMO" name="CUST_MEMO" disabled style="width:480px;;height:30px;"></textarea></td></tr></table>
+		  </div>
+		  <hr style="margin: 5px 0 0 0;border-color:white" >
+		  <label style="display:none;">
+            <span>*제목:<input MaxLength="200" class="required safe" id="Subject" name="Subject" style="width:70%;" type="text" value="<?php echo isset($event)?$event->Subject:"" ?><?php echo $title_G;?>" />(달력에 보일 내용)
+            </span></label>
+            <div id="calendarcolor" style="display:none;">
+            </div>                    
+            <div style="border:1px solid #333333;padding:2px;">
+			<table>
+			<tr>
+			<td>
+			&nbsp;<u>예&nbsp;약&nbsp;일&nbsp;정</u>&nbsp;&nbsp;
               <?php if(isset($event)){
                   $sarr = explode(" ", php2JsTime(mySql2PhpTime($event->StartTime)));
                   $earr = explode(" ", php2JsTime(mySql2PhpTime($event->EndTime)));
               }
-			  ?>                    
+			  ?> 
               <input MaxLength="10" class="required date" id="stpartdate" name="stpartdate" style="padding-left:2px;width:90px;" type="text" value="<?php echo isset($event)?$sarr[0]:""; ?><?php echo $sarr_G[0]; ?>" />                       
               <input MaxLength="5" class="required time" id="stparttime" name="stparttime" style="width:40px;" type="text" value="<?php echo isset($event)?$sarr[1]:""; ?><?php echo $sarr_G[1]; ?>" />To                       
               <input MaxLength="10" class="required date" id="etpartdate" name="etpartdate" style="padding-left:2px;width:90px;" type="text" value="<?php echo isset($event)?$earr[0]:""; ?><?php echo $earr_G[0]; ?>" />                       
               <input MaxLength="50" class="required time" id="etparttime" name="etparttime" style="width:40px;" type="text" value="<?php echo isset($event)?$earr[1]:""; ?><?php echo $earr_G[1]; ?>" />                                            
               <label class="checkp"> 
                 <input id="IsAllDayEvent" name="IsAllDayEvent" type="checkbox" value="1" <?php if(isset($event)&&$event->IsAllDayEvent!=0) {echo "checked";} ?>/>          하루 종일                      
-              </label>                    
+              </label>
+			  </td>
+			  </tr>
+			  </table>
+	<table cellpadding="0" cellspacing="0" border="0" width="550"><tr><td width="200">
+			<table><tr>
+			<td style="width:80px;">&nbsp;예&nbsp;약&nbsp;병&nbsp;원</td>
+			<td>
+				<select name="HOSP_CODE" id="HOSP_CODE" style="width:120px;">
+	    	    <option value="">병원선택</option></select>
+			</td>
+			</tr>
+			<tr>
+			<td style="width:80px;">&nbsp;<u>진&nbsp;&nbsp;&nbsp;료&nbsp;&nbsp;&nbsp;명</u></td>
+			<td>
+				<input type="text" id="RMDY_CODE" name="RMDY_CODE" style="width:120px;" value="<?php echo $event->RMDY_CODE;?>" class="required safe"></input>
+			</td>
+			</tr>
+			<tr>
+			<td style="width:80px;">&nbsp;처&nbsp;&nbsp;&nbsp;치&nbsp;&nbsp;&nbsp;명</td>
+			<td>
+				<input type="text" id="CLNC_CODE" name="CLNC_CODE" style="width:120px;" value="<?php echo $event->CLNC_CODE;?>"></input>
+			</td>
+			</tr>
+			<tr>
+			<td style="width:80px;">&nbsp;원&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;장</td>
+			<td>
+				<select name="RMDY_DOCT" id="RMDY_DOCT" style="width:120px;">
+	    	    <option value="">원장 선택</option>
+			<?php
+			//$row = mysql_fetch_array($rmdy_hd);//원장선택 추가 2013-8-3
+			//$tt=mysql_affected_rows();
+				while($row = mysql_fetch_array($rmdy_hd)){
+					   if($row[1]==$event->RMDY_DOCT){
+						echo "<option value='".$row[1]."' selected>".$row[2]."</option>";
+						}else{
+						echo "<option value='".$row[1]."'>".$row[2]."</option>";
+						}
+					   }
+			?>
+    			</select>
+            </td>
+			</tr>
+			<tr>
+			<td style="width:80px;">&nbsp;관리&nbsp;/&nbsp;장비</td>
+			<td>
+				<select name="ASIN_SEQN" id="ASIN_SEQN" style="width:120px;">
+	    	    <option value="">관리/장비</option></select>
+			</td>
+			</tr>
+			<tr>
+			<td style="width:80px;">&nbsp;예약 접수일</td>
+			<td>
+				<input type="text" style="width:120px;" id="today_YYMD" name="today_YYMD" comment="입력 일이며 내부적으로 처리되므로 서버로 전송되지 않음" disabled value="<?php echo $reged_date;?>"></input>
+			</td>
+			</tr>
+			<tr>
+			<td style="width:80px;">&nbsp;예약 접수자</td>
+			<td>
+				<input type="text" style="width:120px;" id="logged_ID" name="logged_ID" comment="로그인 세션 값" disabled></input>
+			</td>
+			</tr></table>
+		</td>
+		<td style="width:150px;">진료명&nbsp;<input type="text" style="width:60px;" disabled></input>
+				<select style="width:50px;">
+	    	    <option value=""></option></select><hr style="margin: 5px 0 0 0;">
+				<select style="width:160px;" size="9">
+	    	    <option value=""></option></select>
+			</td>
+		<td style="width:150px;">처치명&nbsp;<input type="text" style="width:60px;" disabled>
+				<select style="width:50px;">
+	    	    <option value=""></option></select><hr style="margin: 5px 0 0 0;">
+				<select style="width:160px;" size="9">
+	    	    <option value=""></option></select>
+			</td>
+		</tr></table>
+		<table cellpadding="0" cellspacing="0" border="0" width="550"><tr>
+		<td width="88">&nbsp;&nbsp;예&nbsp;약&nbsp;메&nbsp;모
+		</td>
+		<td>
+		<textarea style="width:97%;" id="RESV_MEMO" name="RESV_MEMO"><?php echo $event->RESV_MEMO;?></textarea>
+		</td>
+		</tr>
+		<tr>
+		<td>&nbsp;&nbsp;예&nbsp;약&nbsp;구&nbsp;분</td>
+<?php
+$cg[0]="";
+$cg[1]="";
+$cg[2]="";
+$cg[$event->CLNC_GUBN]="checked";
+if($event->OPER_CHCK=="Y"){
+	$oc="checked";
+}
+if($event->INET_FLAG){
+	$if="checked";
+}
+if($event->TELE_FLAG){
+	$tf="checked";
+}
+?>
+		<td>
+		[<input type="radio" name="CLNC_GUBN" id="CLNC_GUBN" value="0" <?php echo $cg[0];?> />일반처치 <input type="radio" name="CLNC_GUBN" id="CLNC_GUBN" value="1" <?php echo $cg[1];?> />처치2 <input type="radio" name="CLNC_GUBN" id="CLNC_GUBN" value="2" <?php echo $cg[2];?> />제모] <input type="checkbox" name="OPER_CHCK" id="OPER_CHCK" value="Y" <?php echo $oc;?> />수술 <input type="checkbox" name="INET_FLAG" id="INET_FLAG" value="1" <?php echo $if;?> />채팅 <input type="checkbox" name="TELE_FLAG" id="TELE_FLAG" value="1" <?php echo $tf;?> />아웃바운드콜
+		</td>
+		</tr>
+		</table>
+            <input id="colorvalue" name="colorvalue" type="hidden" value="<?php echo isset($event)?$event->Color:"" ?>" />                
             </div>                
           </label>                 
-          <label>                    
+          <label>
+		  <table border="1" width="500">
+			<tr>
+				<td width="100">진료/처치내역</td><td width="100">예약내역</td><td></td>
+			</tr>
+			<tr>
+				<td colspan="3">
+				<table>
+					<tr>
+						<td>내원병원</td><td>내원일자</td><td>진단명</td><td>처치명</td><td>처치원장</td><td>미등록처치명</td><td>처치직원</td><td>부분</td>
+					</tr>
+					<tr>
+						<td><?php echo $_SESSION['sunap'];?></td><td><?php echo $aclRow[6];?></td><td>진단명</td><td>처치명</td><td>처치원장</td><td>미등록처치명</td><td>처치직원</td><td>부분</td>
+					</tr>
+				</table>
+				</td>
+			</tr>
+			</table>
+
             <span>                        장소:
             </span>                    
             <input MaxLength="200" id="Location" name="Location" style="width:95%;" type="text" value="<?php echo isset($event)?$event->Location:""; ?>" />                 
@@ -267,22 +426,7 @@ if($_GET["id"]){
     $rmdy_hd = mysql_query($rmdy_sql);
 	
 ?>
-            <span>원장:                  
-	    <select name="RMDY_DOCT" id="RMDY_DOCT">
-	    	    <option value="">원장 선택</option>
-<?php
-//$row = mysql_fetch_array($rmdy_hd);//원장선택 추가 2013-8-3
-//$tt=mysql_affected_rows();
-	while($row = mysql_fetch_array($rmdy_hd)){
-		   if($row[1]==$event->RMDY_DOCT){
-			echo "<option value='".$row[1]."' selected>".$row[2]."</option>";
-			}else{
-			echo "<option value='".$row[1]."'>".$row[2]."</option>";
-			}
-		   }
-?>
-    	    </select> 
-            </span>
+           
 			<label>
             <span>                        Remark:
             </span>                    
@@ -310,6 +454,12 @@ if($_GET["id"]){
 				sb.setAttribute('disabled',true);
 				cg="N";
 			}
+		}
+			var cn = document.getElementById("CUST_NAME");
+			var ct = document.getElementById("CUST_TELE");
+			var sj = document.getElementById("Subject");
+		function subj(){//달력에 나올 내용 반영
+			sj.value=cn.value+" "+ct.value;
 		}
 	</script>
 </html>
