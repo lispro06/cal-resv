7b9116971583618115c515dcf35fb46cf6bc68eb
lispro06@gmail.com <lispro06@gmail.com@a97a581a-b179-fca6-b8e4-b75c4b7648a7>
2013-08-08 08:20:36 +0000

    경로 변경에 따른 수정과 db 정보 연동 부분 수정, 고객 번호, 주민번호 출력 부분 수정

--- trunk/cust_srch.php
+++ trunk/cust_srch.php
@@ -1,16 +1,30 @@
 <?php
 header("Content-Type: text/html; charset=UTF-8");
-include_once("php/dbconfig.php");
-include_once("php/functions.php");
+
+   	define('__ZBXE__', true);
+	include("../files/config/db.config.php");
+	$dbname=$db_info->master_db['db_userid'];
+	$dbpass=$db_info->master_db['db_password'];
+class DBConnection{
+	function getConnection($dbname,$dbpass){
+	  //change to your database server/user name/password
+		mysql_connect("localhost",$dbname,$dbpass) or
+         die("Could not connect: " . mysql_error());
+    //change to your database name
+		mysql_select_db($dbname) or 
+		     die("Could not select database: " . mysql_error());
+	}
+}
     $db = new DBConnection();
-    $db->getConnection();
+    $db->getConnection($dbname,$dbpass);
+include_once("functions.php");
     mysql_query("set session character_set_connection=utf8;");
     mysql_query("set session character_set_results=utf8;");
     mysql_query("set session character_set_client=utf8;");
     $sql = "select `CUST_CNUM`, `CUST_NAME`, `CUST_IDEN` from `toto_customer` where `CUST_NAME` LIKE '%" . $_GET["cust_name"]."%'";
     $handle = mysql_query($sql);
 	while($row = mysql_fetch_array($handle)){
-		echo "<a href='#' onclick='in_box(\"".$row[2]."\",\"".$row[1]."\");'>".$row[1]."</a><br />";
+		echo "<a href='#' onclick='in_box(\"".$row[0]."\",\"".$row[1]."\",\"".$row[2]."\");'>".$row[1]."</a> ".$row[2]."<br />";
 	}
 ?>
 <html>
@@ -20,11 +34,13 @@ include_once("php/functions.php");
 //		self.close();
 //	}else{
 //	}
-	var ident=opener.document.getElementById("iden");
+	var cc=opener.document.getElementById("CUST_CNUM");
 	var cn=opener.document.getElementById("CUST_NAME");
-	function in_box(ssn, nm){
-		ident.value = ssn;
-		cn.value = nm;
+	var ci=opener.document.getElementById("CUST_IDEN");
+	function in_box(cnum, name, ssn){
+		cc.value = cnum;
+		cn.value = name;
+		ci.value = ssn;
 		self.close();
 	}
 </script>
